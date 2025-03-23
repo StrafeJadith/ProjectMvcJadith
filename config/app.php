40d493417bb72bf25d99a796717db179ad8af1a6
Jadith Molinares <jmolinaresnavarro@gmail.com>
2025-03-23 16:17:36 -0500

    Subidad de proyecto

---
+++ config/app.php
@@ -0,0 +1,8 @@
+<?php 
+
+    const APP_URL = "http://localhost/ProjectMvc/";
+    const APP_NAME = "PROJECT MVC";
+    const APP_SESSION_NAME = "CRUD";
+
+    date_default_timezone_set("America/Bogota");
+?>
